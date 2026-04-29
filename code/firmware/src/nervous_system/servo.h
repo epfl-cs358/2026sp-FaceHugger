b9c7ea998325c7a3f40012100522a0e698ee5e8f
noa-drn <149162283+noa-drn@users.noreply.github.com>
2026-04-29 12:40:40 +0200

    fix(firmware): pass new angle to map() in setServoAngle, expose getChannel

--- code/firmware/src/nervous_system/servo.h
+++ code/firmware/src/nervous_system/servo.h
@@ -12,12 +12,11 @@ class Servo {
         Servo(Adafruit_PWMServoDriver &pwm, uint8_t pcaChannel, uint16_t servoDefaultAngle);
         void setServoAngle(double angle);
         void returnToDefaultAngle();
+        uint8_t getChannel() const;
     private:
         Adafruit_PWMServoDriver& pwm;
         uint8_t pcaChannel;
         uint16_t servoAngle;
         uint16_t servoDefaultAngle;
-
-
 };
 #endif
