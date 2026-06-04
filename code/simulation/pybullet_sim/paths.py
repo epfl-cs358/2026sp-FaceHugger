619140fd237dce93e44f7910c03f375f4e4382be
Marcus Hamelink <hamelink.m@gmail.com>
2026-06-04 14:26:42 +0200

    chore(sim): extract physics constants into sim_config.yaml

--- code/simulation/pybullet_sim/paths.py
+++ code/simulation/pybullet_sim/paths.py
@@ -14,6 +14,7 @@ SIM_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 GENERATED_DIR = os.path.join(SIM_ROOT, "generated")
 URDF_PATH = os.path.join(GENERATED_DIR, "facehugger.urdf")
 CONFIG_YAML = os.path.join(SIM_ROOT, "facehugger_config.yaml")
+SIM_CONFIG_YAML = os.path.join(SIM_ROOT, "sim_config.yaml")
 FUSION_JSON = os.path.join(GENERATED_DIR, "fusion_export.json")
 MESH_DIR = os.path.join(GENERATED_DIR, "exported_meshes")
 
